c6efb89f5aef42cb217cd13157b6e1f01421b372
juni8835 <3mirabo2909@gmail.com>
2025-08-18 19:14:13 +0900

    250818 jjj

--- resource/dist/app.js
+++ resource/dist/app.js
@@ -1,2 +1,2 @@
-(()=>{console.log("[app.js]");})();
+(()=>{var o=class{constructor(s,e){this._name=s,this._price=e}show(){console.log(`${this._name}\uC758 \uAC00\uACA9\uC740 ${this._price}\uC785\uB2C8\uB2E4!!!`)}info(){console.log(this)}};console.log("[app.js]");var t=new o("\uC0BC\uC131",23);console.log(t);})();
 //# sourceMappingURL=app.js.map
--- resource/js/app.js
+++ resource/js/app.js
@@ -1,1 +1,6 @@
+import Product from './Product';
+
 console.log("[app.js]");
+
+const p = new Product('삼성', 23); 
+console.log(p);
